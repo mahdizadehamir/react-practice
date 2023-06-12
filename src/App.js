088c1ce0d3e3cff996a9a9c763e3677b82471e0a
Amir <mehdizadeh.amir11@gmail.com>
2023-06-12 17:51:08 +0330

    navigte to notFound

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Landing from "./components/Landing";
 import Products from "./components/Products";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import AboutUs from "./components/AboutUs";
 import DetailedPage from "./components/DetailedPage";
+import NotFound from "./components/NotFound";
 export default class App extends Component {
   render() {
     return (
@@ -17,6 +18,8 @@ export default class App extends Component {
           <Route path="/products" element={<Products />} />
           <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/products/:id" element={<DetailedPage />} />
+          <Route path="/notfound" element={<NotFound />} />
+          <Route path="/*" element={<Navigate to="/notfound" />} />
         </Routes>
         <Footer />
       </div>
